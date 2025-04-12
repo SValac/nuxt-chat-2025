d027f2f95d7b83b70ef0d19498c7b90c1797e3b4
SValac <shiro.valac@gmail.com>
2025-04-12 02:56:33 -0600

    Implement chat interface with ChatInput and ChatWindow components, add chat scrolling logic, and clean up the main chat page

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,12 +1,5 @@
 <script setup></script>
 
 <template>
-	<UContainer class="page-container">
-		<h1 class="title">Chat</h1>
-		<UButton
-			class="button"
-			to="/"
-			label="Home"
-		/>
-	</UContainer>
+	<ChatWindow />
 </template>
